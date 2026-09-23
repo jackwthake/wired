d8e4c83eec6dd076e85498ad20ca6ff000f5dcec
jackwthake <jackthake@hotmail.com>
2026-09-23 01:46:54 -0700

    Updated cmake to not build superfluous pieces of SDL

--- src/navi/widgets.c
+++
@@ -1,5 +0,0 @@
-#include "widgets.h"
-
-#include <navi/navi.h>
-#include <gfx.h>
-
--- src/navi/widgets.h
+++
@@ -1,8 +0,0 @@
-#ifndef __WIDGETS_H__
-#define __WIDGETS_H__
-
-struct widget {
-  unsigned x, y, w, h;
-};
-
-#endif // __WIDGETS_H__
